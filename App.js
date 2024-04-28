8e9c8115e2d9c799189423b2386be9916ebbc551
Yaw Mireku <ymir8091@gmail.com>
2024-04-27 21:30:39 -0400

    Set up basic navigation

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image , Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-// import ProfileScreen from './pages/ProfileView'
-// import StatsView from './pages/StatsView'
-// import HomeView from './pages/HomeView'
-// import ClothingView from './pages/ClothingView'
-// import BudgetView from './pages/BudgetView'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfileView from './pages/ProfileView';
+import ClothingView from './pages/ClothingView';
+import OutfitsView from './pages/OutfitsView';
+import ExpensesView from './pages/ExpensesView';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,16 +13,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Welcome'}}
-        />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="HomeView" component={HomeView} />
-        <Stack.Screen name="Stats" component={StatsView} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome!' }} />
+        <Stack.Screen name="Profile" component={ProfileView} />
         <Stack.Screen name="Clothing" component={ClothingView} />
-        <Stack.Screen name="Budget" component={BudgetView} /> */}
+        <Stack.Screen name="Outfits" component={OutfitsView} />
+        <Stack.Screen name="Expenses" component={ExpensesView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -36,43 +29,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  buttonContainer: {
+    marginBottom: 12
   },
 });
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <>
-    <Button
-      title="Go to Jane's profile"
-      // onPress={() =>
-      //   navigation.navigate('Profile', {name: 'Jane'})
-      // }
-    />
-    <Button
-      title="Go to Jane's Stats"
-      // onPress={() =>
-      //   navigation.navigate('Stats', {name: 'Jane'})
-      // }
-    />
-    <Button
-      title="Go to Jane's Home"
-      // onPress={() =>
-      //   navigation.navigate('HomeView', {name: 'Jane'})
-      // }
-    />
-    <Button
-      title="Go to Jane's clothing"
-      // onPress={() =>
-      //   navigation.navigate('Clothing', {name: 'Jane'})
-      // }
-    />
-    <Button
-      title="Go to Jane's budget"
-      // onPress={() =>
-      //   navigation.navigate('Budget', {name: 'Jane'})
-      // }
-     /> 
-    </>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Profile" onPress={() => navigation.navigate('Profile')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Clothing" onPress={() => navigation.navigate('Clothing')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Outfits" onPress={() => navigation.navigate('Outfits')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Expenses" onPress={() => navigation.navigate('Expenses')} />
+      </View>
+    </View>
   );
 };
-
